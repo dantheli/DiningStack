8c574ce2d9b4d3509619a848aa3958eb27e03c8b
Alexander Zielenski <alex@alexzielenski.com>
2015-10-28 20:14:41 -0400

    Initial Commit

---
+++ DiningStack/DiningStack.h
@@ -0,0 +1,19 @@
+//
+//  DiningStack.h
+//  DiningStack
+//
+//  Created by Alexander Zielenski on 10/28/15.
+//  Copyright © 2015 Cornell App Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiningStack.
+FOUNDATION_EXPORT double DiningStackVersionNumber;
+
+//! Project version string for DiningStack.
+FOUNDATION_EXPORT const unsigned char DiningStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiningStack/PublicHeader.h>
+
+
